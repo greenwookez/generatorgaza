fdbfa44f32c9e790448c7af14e56abd2c17ffdb6
Bogdan Bogdanov <greenwookez@gmail.com>
2025-11-01 13:21:55 +0200

    feat: tailwind v4, shadcn init

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -1,3 +1,12 @@
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+
 export default async function HomePage() {
-  return <div className="text-3xl text-red-500">Hello World</div>
+  return (
+    <div className="flex flex-col gap-4 p-8 max-w-[350px]">
+      <Button>Default</Button>
+      <Button size="sm">Small</Button>
+      <Input placeholder="Type to search..." />
+    </div>
+  )
 }
