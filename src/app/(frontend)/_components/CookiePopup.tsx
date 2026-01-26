dc872f4e556d246f4eee7fa257e880455eaa1a20
Bogdan Bogdanov <greenwookez@gmail.com>
2026-01-26 20:39:00 +0200

    feat: cookie banner polishing

--- src/app/(frontend)/_components/CookiePopup.tsx
+++ src/app/(frontend)/_components/CookiePopup.tsx
@@ -2,42 +2,36 @@
 
 import { Button } from '@/components/ui/button'
 import IsLocalStorageAvailable from '@/lib/isLocalStorageAvailable'
+import Link from 'next/link'
 import { useEffect, useState } from 'react'
 
 export const CookiePopup = () => {
-  const [shouldShowCookies, setShouldShowCookies] = useState(
-    IsLocalStorageAvailable() ? localStorage.getItem('cookie-consent') !== 'agreed' : true,
-  )
+  const [show, setShow] = useState(false)
 
   const onAcceptClick = () => {
-    console.log('onAcceptClick')
     if (IsLocalStorageAvailable()) {
-      console.log('Accepted')
       localStorage.setItem('cookie-consent', 'agreed')
-      setShouldShowCookies(false)
+      setShow(false)
     }
   }
 
   useEffect(() => {
-    const listener = (e: MouseEvent) => {
-      console.log('click', e.target)
-    }
-
-    window.addEventListener('click', listener)
-    return () => {
-      window.removeEventListener('click', listener)
+    if (IsLocalStorageAvailable()) {
+      setShow(localStorage.getItem('cookie-consent') !== 'agreed')
     }
   }, [])
 
-  if (!shouldShowCookies) {
-    return null
-  }
+  if (!show) return null
 
   return (
-    <div className="absolute bottom-4 left-3 bg-background py-4 px-5 w-[480px] shadow-[0px_5px_20px_0px_rgba(0,31,84,0.08)] rounded-[16px] flex gap-x-3 items-center text-muted-foreground text-[0.75rem] leading-[1.125rem]">
+    <div className="z-50 fixed bottom-4 left-3 max-sm:right-3 max-sm:flex-col max-sm:items-start sm:w-[480px] bg-background py-4 px-5  shadow-[0px_5px_20px_0px_rgba(0,31,84,0.08)] rounded-[16px] flex gap-3 items-center text-muted-foreground text-[0.75rem] leading-[1.125rem] max-sm:animate-cookie-appear-bottom sm:animate-cookie-appear-left">
       <span>
-        Мы обрабатываем cookies, чтобы сделать наш сайт удобнее и персонализированее для вас.
-        Подробнее: политика использования cookies и защита данных.
+        Мы используем Cookie, чтобы сделать наш сайт удобнее и персонализированее для вас.
+        Подробнее:{' '}
+        <Link href="#" className="underline" prefetch>
+          политика обработки персональных данных
+        </Link>
+        .
       </span>
       <Button onClick={onAcceptClick}>Принять</Button>
     </div>
